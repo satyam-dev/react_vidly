3519aea7984aebb09e1fe7727eeb105552a0c601
Satyam Saluja <satyam.15bcs1171@abes.ac.in>
2020-05-22 03:09:14 +0530

    extracted form class

---
+++ src/components/common/form.jsx
@@ -0,0 +1,48 @@
+import { Component } from "react";
+import Joi from "joi-browser";
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {},
+  };
+  handleChange = ({ currentTarget: input }) => {
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = this.validateProperty(input);
+    else delete errors[input.name];
+    this.setState({ data, errors });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+    this.doSubmit();
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const result = Joi.validate(this.state.data, this.schema, options);
+    if (!result.error) return null;
+    let errors = {};
+    errors = result.error.details
+      .map((detail) => {
+        return { [detail.path[0]]: detail.message };
+      })
+      .reduce((a, b) => {
+        return { ...a, ...b };
+      });
+    return Object.keys(errors).length ? errors : null;
+  };
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+}
+
+export default Form;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Input from "./common/input";
 import Joi from "joi-browser";
-class LoginForm extends Component {
+import Form from "./common/form";
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
-    errors: {}, // keys as account keys, values as error string
+    data: { username: "", password: "" },
+    errors: {}, // keys as data keys, values as error string
   };
 
   schema = {
@@ -12,47 +13,14 @@ class LoginForm extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = this.validateProperty(input);
-    else delete errors[input.name];
-    this.setState({ account, errors });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
+  doSubmit = (e) => {
+    //submit
+    console.log("Submitted");
     // Call database request
-  };
-
-  validate = () => {
-    const options = { abortEarly: false };
-    const result = Joi.validate(this.state.account, this.schema, options);
-    if (!result.error) return null;
-    let errors = {};
-    errors = result.error.details
-      .map((detail) => {
-        return { [detail.path[0]]: detail.message };
-      })
-      .reduce((a, b) => {
-        return { ...a, ...b };
-      });
-    return Object.keys(errors).length ? errors : null;
-  };
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
   };
   render() {
     const {
-      account: { username, password },
+      data: { username, password },
       errors,
     } = this.state;
     return (
